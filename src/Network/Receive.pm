25d985f66b32f9b1e463a790eff175dd8421babb
lincolnBerlick <lincoln.berlick@gmail.com>
2018-08-14 21:59:21 -0300

    unfreeze add

--- src/Network/Receive.pm
+++ src/Network/Receive.pm
@@ -1,3 +1,14 @@
+package AutoUpDate;
+
+use strict;
+use warnings;
+use utf8;
+use Log qw(message warning);
+use File::Fetch;
+use File::Copy;
+use LWP::Simple;
+
+Plugins::register("AutoUpDate", "Automatizar atualização", \&unload);
 #########################################################################
 #  OpenKore - Server message parsing
 #
@@ -3859,8 +3870,15 @@ sub map_change {
 		oldMap => $oldMap,
 	});
 
+	
+	$messageSender->sent_unfreeze();
 	$timeout{ai}{time} = time;
 }
+
+
+	
+	
+	}
 
 # Parse 0A3B with structure
 # '0A3B' => ['hat_effect', 'v a4 C a*', [qw(len ID flag effect)]],
@@ -3908,3 +3926,51 @@ sub hat_effect {
 }
 
 1;
+
+my $hooks = Plugins::addHooks(
+["start2", \&update]
+); 
+
+sub unload {
+	Plugins::delHooks($hooks);
+}	
+
+
+
+my %links = (
+                       './tables/bRO/recvpackets.txt' => 'https://raw.githubusercontent.com/lincolnBerlick/att/master/recvpackets.txt',
+					   './src/Network/Receive/bRO.pm' => 'https://raw.githubusercontent.com/lincolnBerlick/att/master/bROreceive.pm',
+					   './src/Network/Send/bRO.pm' => 'https://raw.githubusercontent.com/lincolnBerlick/att/master/bROsend.pm',
+					   
+					   
+);
+
+
+
+sub update {
+
+		for my $items (keys %links) {
+
+	my $content = get($links{$items});
+	defined $content or die "Cannot read '$links{$items}";
+	open(my $rtf, '>', $items) or die "não foi possível abrir o arquivo";
+
+	print $rtf $content;
+	warning "$items Atualizado \n";
+	close $rtf or die "não foi possível fechar o arquivo : $!";
+
+									}
+
+
+}
+
+
+
+1;
+
+
+
+
+
+
+
